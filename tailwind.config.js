35ed2438d0d98ff2cab8e4e7494e85de09d3b399
Yosevu Kilonzo <yosevu@yosevu.com>
2026-01-17 19:47:22 -0600

    chore: migrate to npm and modernize tooling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./content-script/src/**/*.{js,ts,jsx,tsx}"
+    "./lib/**/*.{js,ts,jsx,tsx}",
+    "./content-script/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        'spin-slow': 'spin 20s linear infinite',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
